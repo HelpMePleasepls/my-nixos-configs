91d70d6d5022f2c50480cd6301c3f1dabfa3222d
Joeff <derpyface321steam@gmail.com>
2025-01-12 13:59:16 +0200

    Update configuration.nix

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,131 +1,112 @@
 { config, pkgs, ... }:
+
+{
   # Define package collections/sets based on categories
-  let
-    developmentPackages = with pkgs; [
-      python314
-      nodejs_23
-      gcc
-      go
-      php
-      cargo
-      zulu
-      ruby
-      julia
-      python313Packages.pip
-      tree-sitter
-    ];
+  environment.systemPackages = with pkgs; [
+    # System Tools
+    btop
+    nvtop
+    git
+    lazygit
+    neovim
+    wget
+    tree
+    tmux
+    ripgrep
+    fd
+    fzf
+    fastfetch
+    swtpm
+    libGLU
+    libGL
+    mesa
+    libvpx
+    libva-utils
+    ethtool
+    lua54Packages.luarocks
+    lrzip
+    xclip
+    viu
+    chafa
+    ueberzugpp
+    unzip
+    unrar
+    rar
+    zip
 
-    mediaPackages = with pkgs; [
-      davinci-resolve
-      ffmpeg
-      imagemagick
-      vlc
-      mpv
-      obs-studio
-      gpu-screen-recorder
-      gpu-screen-recorder-gtk
-      qbittorrent
-      rclone
-      ascii-image-converter
-      inotify-tools
-      inotify-info
-      asciiquarium
-      lrzip
-      xclip
-      viu
-      chafa
-      ueberzugpp
-      unzip
-      unrar
-      rar
-      zip
-      easyeffects
-      audio-sharing
-      qpwgraph
-      ungoogled-chromium
-    ];
+    # Development Packages
+    python314
+    nodejs_23
+    gcc
+    go
+    php
+    cargo
+    zulu
+    ruby
+    julia
+    python313Packages.pip
+    tree-sitter
 
-    kdePackages = with pkgs; [
-      kdePackages.kwin
-      ksnip
-      kdePackages.spectacle
-      kdePackages.knotifications
-      kdePackages.plasma-vault
-      kdePackages.discover
-      kdePackages.plasma-browser-integration
-      menulibre
-      mission-center
-    ];
+    # KDE Packages
+    kdePackages.kwin
+    ksnip
+    kdePackages.spectacle
+    kdePackages.knotifications
+    kdePackages.plasma-vault
+    kdePackages.discover
+    kdePackages.plasma-browser-integration
+    menulibre
+    mission-center
+];
 
-    securityPackages = with pkgs; [
-      bitwarden-desktop
-      tomb
-      wireguard-tools
-    ];
-
-    gamingPackages = with pkgs; [
-      steamtinkerlaunch
-      protonup-qt
-      protontricks
-      lutris
-      bottles
-      gamemode
-      gamescope
-      mangohud
-      goverlay
-      alvr
-      adwsteamgtk
-      onboard
-    ];
-
-    communicationPackages = with pkgs; [
-      vesktop
-      thunderbird
-      protonmail-bridge
-      protonmail-desktop
-      zapzap
-      signal-desktop
-    ];
-
-    systemTools = with pkgs; [
-      btop
-      nvtop
-      git
-      lazygit
-      neovim
-      wget
-      tree
-      tmux
-      ripgrep
-      fd
-      fzf
-      fastfetch
-      swtpm
-      libGLU
-      libGL
-      mesa
-      libvpx
-      libva-utils
-      ethtool
-      lua54Packages.luarocks
-    ];
-
-    officePackages = with pkgs; [
-      libreoffice-qt6-fresh
-      onlyoffice-desktopeditors
-    ];
-
-  in {
-    # Apply to user packages
-    users.users.bob.packages =
-      developmentPackages ++
-      mediaPackages ++
-      gamingPackages ++
-      communicationPackages ++
-      securityPackages ++
-      kdePackages ++
-      officePackages;
-
-    # Apply to system packages
-    environment.systemPackages = systemTools;
-  }
+  # If you need to specify user-specific packages, you can do it this way:
+  users.users.bob.packages = with pkgs; [
+    # Add any user-specific packages here
+    # Communication Packages
+    vesktop
+    thunderbird
+    protonmail-bridge
+    protonmail-desktop
+    zapzap
+    signal-desktop
+    # Gaming Packages
+    steamtinkerlaunch
+    protonup-qt
+    protontricks
+    lutris
+    bottles
+    gamemode
+    gamescope
+    mangohud
+    goverlay
+    alvr
+    adwsteamgtk
+    onboard
+    # Security Packages
+    bitwarden-desktop
+    tomb
+    wireguard-tools
+    # Office Packages
+    libreoffice-qt6-fresh
+    onlyoffice-desktopeditors
+    # Media Packages
+    davinci-resolve
+    ffmpeg
+    imagemagick
+    vlc
+    mpv
+    obs-studio
+    gpu-screen-recorder
+    gpu-screen-recorder-gtk
+    qbittorrent
+    rclone
+    ascii-image-converter
+    inotify-tools
+    inotify-info
+    asciiquarium
+    easyeffects
+    audio-sharing
+    qpwgraph
+    ungoogled-chromium
+  ];
+}
