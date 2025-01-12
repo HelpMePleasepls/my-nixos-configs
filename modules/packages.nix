f45f6a6f7055f0bddf02bbb0dfcc10a933933e6e
Joeff <derpyface321steam@gmail.com>
2025-01-12 12:58:11 +0200

    Update configuration.nix

---
+++ modules/packages.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, ... }:
+
+{
+  # Define package collections/sets based on categories
+  let
+    developmentPackages = with pkgs; [
+      python314
+      nodejs_23
+      gcc
+      go
+      php
+      cargo
+      zulu
+      ruby
+      julia
+      python313Packages.pip
+      tree-sitter
+    ];
+
+    mediaPackages = with pkgs; [
+      davinci-resolve
+      ffmpeg
+      imagemagick
+      vlc
+      mpv
+      obs-studio
+      gpu-screen-recorder
+      gpu-screen-recorder-gtk
+    ];
+
+    gamingPackages = with pkgs; [
+      steamtinkerlaunch
+      protonup-qt
+      protontricks
+      lutris
+      bottles
+      gamemode
+      gamescope
+      mangohud
+      goverlay
+    ];
+
+    communicationPackages = with pkgs; [
+      vesktop
+      thunderbird
+      protonmail-bridge
+      protonmail-desktop
+      zapzap
+      signal-desktop
+    ];
+
+    systemTools = with pkgs; [
+      btop
+      nvtop
+      git
+      lazygit
+      neovim
+      wget
+      tree
+      tmux
+      ripgrep
+      fd
+      fzf
+      fastfetch
+    ];
+
+    officePackages = with pkgs; [
+      libreoffice-qt6-fresh
+      onlyoffice-desktopeditors
+    ];
+
+  in {
+    # Apply to user packages
+    users.users.bob.packages =
+      developmentPackages ++
+      mediaPackages ++
+      gamingPackages ++
+      communicationPackages ++
+      officePackages;
+
+    # Apply to system packages
+    environment.systemPackages = systemTools;
+  };
+}
