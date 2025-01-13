bb5847915f2c6612fdca779e8215e94005bba82b
Joeff <derpyface321steam@gmail.com>
2025-01-13 21:01:28 +0200

    Automated update

---
+++ custom-nixpkgs/img2wav.nix
@@ -0,0 +1,33 @@
+{ lib
+, python3Packages
+, fetchFromGitHub
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "img2wav";
+  version = "0.1.2";
+
+  src = fetchFromGitHub {
+    owner = "shareef12";
+    repo = "img2wav";
+    rev = "v${version}";
+    sha256 = "sha256-YW4MHSraIzSW2MuayEHl+4Y1GPN+xdq6G00PnsdjST8=";  # Updated hash from the error message
+  };
+
+  propagatedBuildInputs = with python3Packages; [
+    numpy
+    pillow
+  ];
+
+  # The package doesn't have any tests
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Convert images to wav audio files to view in a spectrogram";
+    homepage = "https://github.com/shareef12/img2wav";
+    license = licenses.gpl2;
+    maintainers = [ ];
+    platforms = platforms.all;
+  };
+}
+
