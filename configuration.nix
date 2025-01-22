a3c0c2c6a92fb74b23b5e77b8836c1303359e53e
Joeff <derpyface321steam@gmail.com>
2025-01-22 21:54:16 +0200

    Automated update

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   # Use the systemd-boot EFI boot loader.
 boot = {
   tmpOnTmpfs = true; # faster build times, higher RAM usage
-  tmpOnTmpfsSize = "10G";
+  tmpOnTmpfsSize = "20G";
   loader = {
   systemd-boot.enable = true;
   efi.canTouchEfiVariables = true;
