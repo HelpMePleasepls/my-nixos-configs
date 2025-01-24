4b7c609ae2d1d836bafa4fca43e549ad12359c69
Joeff <derpyface321steam@gmail.com>
2025-01-24 02:26:36 +0200

    Automated update

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # ./modules/packages.nix
+      ./modules/packages.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
