349b125dd47f6f558317dc1d725015e1679f5c32
Joeff <derpyface321steam@gmail.com>
2025-01-24 12:33:52 +0200

    Automated update

--- modules/packages.nix
+++ modules/packages.nix
@@ -75,13 +75,59 @@
     signal-desktop
     discord-canary
 
+    # Gaming Packages
+    prismlauncher
+    steamtinkerlaunch
+    protonup-qt
+    protontricks
+    wineWow64Packages.full
+    winePackages.wayland
+    lutris
+    bottles
+    gamescope
+    mangohud
+    alvr
+    mangojuice
+    adwsteamgtk
+    onboard
+
     # Security Packages
     bitwarden-desktop
     tomb
     wireguard-tools
+
     # Office Packages
     libreoffice-qt6-fresh
     onlyoffice-desktopeditors
+
+    # Media Packages
+    davinci-resolve
+    yt-dlp
+    revolt-desktop
+    drawio
+    upscayl
+    losslesscut-bin
+    ffmpeg
+    imagemagick
+    libjxl
+    vlc
+    mpv
+    obs-studio
+    gpu-screen-recorder
+    gpu-screen-recorder-gtk
+    gimp
+    (callPackage /home/bob/nixos-config/custom-nixpkgs/img2wav.nix {})
+    audacity
+    qbittorrent
+    rclone
+    ascii-image-converter
+    inotify-tools
+    inotify-info
+    asciiquarium
+    easyeffects
+    audio-sharing
+    qpwgraph
+    ungoogled-chromium
 
     # Other packages
     kicad
